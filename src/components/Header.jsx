9b9efbc83805b66e199312340b2ff0b20c5c6025
Glory-mmachi <105730131+Glory-mmachi@users.noreply.github.com>
2024-02-03 14:16:17 +0100

    change project name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,39 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Hero from "./Hero";
 import Nav from "./Nav";
 import { useTheme } from "../../ContextApi";
 
 export default function Header() {
   const { setSearchKey } = useTheme();
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const searchMovies = (e) => {
     e.preventDefault();
     setSearchKey(searchValue);
-    setSearchValue('');
+    setSearchValue("");
   };
 
   const handleInputChange = (e) => {
     setSearchValue(e.target.value);
   };
-  
+
   return (
     <>
       <Nav />
       <div className="header container">
         {/* header section */}
-        <h2>MovieApp</h2>
+        <h2>MovieLog</h2>
         <form onSubmit={searchMovies}>
-          <input 
+          <input
             type="text"
             placeholder="Search Movies"
             value={searchValue}
             onChange={handleInputChange}
             name="searchInput"
           />
-          <button type="submit" className="btn btn-search">Search</button>
+          <button type="submit" className="btn btn-search">
+            Search
+          </button>
         </form>
       </div>
       <Hero />
