a80ca1c5a93e789c4e67df06cbeb531945e47941
Glory-mmachi <105730131+Glory-mmachi@users.noreply.github.com>
2024-02-03 14:14:42 +0100

    update search functionality

--- ContextApi.jsx
+++ ContextApi.jsx
@@ -7,13 +7,15 @@ export default function ThemeProvider({ children }) {
   const [movies, setMovie] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState([]);
   const [darkMode, setDarkMode] = useState(true);
-  const [searchKey,setSearchKey]=useState('')
+  const [searchKey, setSearchKey] = useState("");
   const url = "https://api.themoviedb.org/3";
   // https://api.themoviedb.org/3/discover/movie
   // https://api.themoviedb.org/3/search/movie?api_key=322b422e6361cbc7abf4bfb1ad497d15
 
   const fetchMovies = async (type) => {
-    const res = await fetch(`${url}${type}?api_key=${api_key}&query='${searchKey}'`);
+    const res = await fetch(
+      `${url}${type}?api_key=${api_key}&query=${searchKey}`
+    );
     const data = await res.json();
     setMovie(data.results);
     setSelectedMovie(data.results[0]);
@@ -24,13 +26,21 @@ export default function ThemeProvider({ children }) {
   }, []);
 
   const toggleTheme = () => {
-    setDarkMode(!darkMode)
-    document.body.classList.toggle('dark',darkMode);
+    setDarkMode(!darkMode);
+    document.body.classList.toggle("dark", darkMode);
   };
 
   return (
     <ThemeContext.Provider
-      value={{ movies, fetchMovies, selectedMovie, toggleTheme, darkMode ,setSearchKey,searchKey}}
+      value={{
+        movies,
+        fetchMovies,
+        selectedMovie,
+        toggleTheme,
+        darkMode,
+        setSearchKey,
+        searchKey,
+      }}
     >
       {children}
     </ThemeContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import { useTheme } from "../../ContextApi";
 import MovieList from "../components/MovieList";
 
 export default function Home() {
-  const {  fetchMovies } = useTheme();
-  // const type = "/discover/movie";
-  const type = "/trending/all/day";
+  const { fetchMovies, searchKey } = useTheme();
+  const type = searchKey ? "/search/movie" : "/trending/all/day";
+  fetchMovies(type, searchKey);
 
   fetchMovies(type);
   return (
-   <div>
-    <MovieList/>
-   </div>
+    <div>
+      <MovieList />
+    </div>
   );
 }
